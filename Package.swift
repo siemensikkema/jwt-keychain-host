641780aef1ee7cf310457011cef6cba9af7cbcf1
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2017-09-01 14:15:46 +0200

    "created jwt-keychain-host from template https://github.com/nodes-vapor/template"

---
+++ Package.swift
@@ -0,0 +1,25 @@
+import PackageDescription
+
+let package = Package(
+    name: "jwt-keychain-host",
+    targets: [
+        Target(name: "App"),
+        Target(name: "Run", dependencies: ["App"]),
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/redis-provider.git", majorVersion: 2),
+        .Package(url: "https://github.com/nodes-vapor/bugsnag.git", majorVersion: 1),
+        .Package(url: "https://github.com/nodes-vapor/sugar", majorVersion: 2)
+    ],
+    exclude: [
+        "Config",
+        "Database",
+        "Localization",
+        "Public",
+        "Resources",
+    ]
+)
+
